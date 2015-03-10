91ee4be4ea6a86ad94baf12413b34f3045d39ca0
zicog <michael@debian>
2015-03-10 15:39:11 +0200

    jslint clean up

--- pasm.js
+++ pasm.js
@@ -3,22 +3,22 @@
 //
 // Command line assembler for PASM.
 //
-"use strict"
+"use strict";
 
 var PEG = require('pegjs');
 var assert = require('assert');
 var fs = require('fs');
 
 // Read pegjs grammar files and concatenate. 
-function loadGrammar () {
+function loadGrammar() {
     var grammarFiles = [
             "pre.peg",
             "con.peg",
             "dat.peg"
         ],
         grammar = "";
-    grammarFiles.map(function(name) {
-        grammar += fs.readFileSync(name, 'utf-8') ;
+    grammarFiles.map(function (name) {
+        grammar += fs.readFileSync(name, 'utf-8');
     });
     return grammar;
 }
@@ -36,25 +36,25 @@ var source = fs.readFileSync('con.spin', 'utf-8');
 
 // Do a test
 var ast;
-   try {
-        ast = parser.parse(source, {
-            startRule: "start"
-        });
-        console.log("Parse OK");
-        console.log(typeof j);
-        console.log(JSON.stringify(ast, null, '  '));
-        //assert.deepEqual( parse("x==1\n"), ["a", "b", "c"] );
-    } catch (e) {
-        console.log("Ooops...syntax error");
-        console.log(e.message);
-        console.log("    Offset:  " + e.offset);
-        console.log("    Line:    " + e.line);
-        console.log("    Column:  " + e.column);
-        // console.log("    Expected:" + e.expected);
-        console.log("    Found:   " + e.found);
-    }
 
-function testParser (s) {
+try {
+    ast = parser.parse(source, {
+        startRule: "start"
+    });
+    console.log("Parse OK");
+    console.log(JSON.stringify(ast, null, '  '));
+    //assert.deepEqual( parse("x==1\n"), ["a", "b", "c"] );
+} catch (e) {
+    console.log("Ooops...syntax error");
+    console.log(e.message);
+    console.log("    Offset:  " + e.offset);
+    console.log("    Line:    " + e.line);
+    console.log("    Column:  " + e.column);
+    // console.log("    Expected:" + e.expected);
+    console.log("    Found:   " + e.found);
+}
+
+function testParser(s) {
     var j;
     try {
         j = parser.parse(s, {
@@ -75,12 +75,8 @@ function testParser (s) {
 }
 
 fs.readFile("dat.spin", {encoding: "utf8"}, function (err, data) {
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
     testParser(data);
 });
-
-
-
-
-
-
